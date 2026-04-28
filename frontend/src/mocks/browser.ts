85da5ba8acdf76bba630520b82114618ce906267
Andrea Rossi <andre.reds.11@gmail.com>
2026-04-28 19:51:32 +0200

    feat: add user mocks to worker

--- frontend/src/mocks/browser.ts
+++ frontend/src/mocks/browser.ts
@@ -1,5 +1,10 @@
 import { setupWorker } from "msw/browser";
 import { authHandlers } from "./handlers/auth.handlers";
 import { aiHandlers } from "./handlers/ai.handlers";
+import { userHandlers } from "./handlers/user.handler";
 
-export const worker = setupWorker(...authHandlers, ...aiHandlers);
+export const worker = setupWorker(
+  ...authHandlers,
+  ...aiHandlers,
+  ...userHandlers,
+);

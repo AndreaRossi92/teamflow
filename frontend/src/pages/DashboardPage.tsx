ad5083d497b103115554a6505efd4f065693de39
Andrea Rossi <andre.reds.11@gmail.com>
2026-05-28 20:26:45 +0200

    feat: Hide dashboard sections based on user role

--- frontend/src/pages/DashboardPage.tsx
+++ frontend/src/pages/DashboardPage.tsx
@@ -8,41 +8,47 @@ import {
 } from "@mui/material";
 import { useTranslation } from "react-i18next";
 import { useNavigate } from "react-router-dom";
+import { useAuth } from "../providers/useAuth";
 
 export default function DashboardPage() {
   const { t } = useTranslation("common");
+  const { user } = useAuth();
   const navigate = useNavigate();
 
   return (
     <Grid container spacing={2}>
-      <Grid size={{ xs: 12, md: 6 }}>
-        <Card>
-          <CardActionArea
-            onClick={() => {
-              navigate("/ai");
-            }}
-          >
-            <CardHeader title={t("AI")} />
-            <CardContent>
-              <Alert severity="success">{t("openSection")}</Alert>
-            </CardContent>
-          </CardActionArea>
-        </Card>
-      </Grid>
-      <Grid size={{ xs: 12, md: 6 }}>
-        <Card>
-          <CardActionArea
-            onClick={() => {
-              navigate("/users");
-            }}
-          >
-            <CardHeader title={t("users")} />
-            <CardContent>
-              <Alert severity="success">{t("openSection")}</Alert>
-            </CardContent>
-          </CardActionArea>
-        </Card>
-      </Grid>
+      {user?.role === "admin" && (
+        <Grid size={{ xs: 12, md: 6 }}>
+          <Card>
+            <CardActionArea
+              onClick={() => {
+                navigate("/ai");
+              }}
+            >
+              <CardHeader title={t("AI")} />
+              <CardContent>
+                <Alert severity="success">{t("openSection")}</Alert>
+              </CardContent>
+            </CardActionArea>
+          </Card>
+        </Grid>
+      )}
+      {user?.role === "admin" && (
+        <Grid size={{ xs: 12, md: 6 }}>
+          <Card>
+            <CardActionArea
+              onClick={() => {
+                navigate("/users");
+              }}
+            >
+              <CardHeader title={t("users")} />
+              <CardContent>
+                <Alert severity="success">{t("openSection")}</Alert>
+              </CardContent>
+            </CardActionArea>
+          </Card>
+        </Grid>
+      )}
       <Grid size={{ xs: 12, md: 6 }}>
         <Card sx={{ opacity: 0.5 }}>
           <CardHeader title={t("projects")} />

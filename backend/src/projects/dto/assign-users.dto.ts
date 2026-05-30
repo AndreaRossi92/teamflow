531e454bf049490ed5ba255c93a68dacba6a53a2
Andrea Rossi <andre.reds.11@gmail.com>
2026-05-31 01:32:54 +0200

    feat: accept empty assigned users

--- backend/src/projects/dto/assign-users.dto.ts
+++ backend/src/projects/dto/assign-users.dto.ts
@@ -1,10 +1,9 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { ArrayNotEmpty, IsArray, IsUUID } from 'class-validator';
+import { IsArray, IsUUID } from 'class-validator';
 
 export class AssignUsersDto {
   @ApiProperty({ example: ['uuid-1', 'uuid-2'], type: [String] })
   @IsArray()
-  @ArrayNotEmpty()
   @IsUUID('4', { each: true })
   userIds!: string[];
 }

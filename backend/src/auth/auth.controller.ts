4ffd276b7e9934029702c7555067883161845d35
Andrea Rossi <andre.reds.11@gmail.com>
2026-05-31 23:02:15 +0200

    chore: remove unused import

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -16,7 +16,7 @@ import { JwtRefreshGuard } from './guards/jwt-refresh.guard';
 import { JwtGuard } from './guards/jwt.guard';
 import { ChangePasswordDto } from './dto/change-password.dto';
 import { JwtUser } from './strategies/jwt.strategy';
-import { Throttle, ThrottlerGuard } from '@nestjs/throttler';
+import { ThrottlerGuard } from '@nestjs/throttler';
 
 const COOKIE_BASE = {
   httpOnly: true,

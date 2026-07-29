dbdba439652a4ce02ad9414f17be0f54e824656c
Andrea Rossi <andre.reds.11@gmail.com>
2026-07-29 19:27:00 +0200

    feat: create `Dot` component

---
+++ frontend/src/components/Dot.tsx
@@ -0,0 +1,21 @@
+import { Box, Tooltip } from "@mui/material";
+
+type Dot = {
+  title?: string;
+  color: string;
+};
+
+export default function Dot({ title, color }: Dot) {
+  return (
+    <Tooltip title={title}>
+      <Box
+        sx={{
+          width: 10,
+          height: 10,
+          borderRadius: "50%",
+          bgcolor: color,
+        }}
+      />
+    </Tooltip>
+  );
+}
